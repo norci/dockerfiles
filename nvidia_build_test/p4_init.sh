9cb5ff54e572ab4f254765e12c9d4aa0a691e60e
Your Name <you@example.com>
2019-05-23 21:25:39 +0800

    updated p4_init.sh

--- nvidia_build_test/p4_init.sh
+++ nvidia_build_test/p4_init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eux
 # docker-compose up -d
-docker-compose exec perforce p4 login
+# docker-compose exec perforce p4 login
+docker-compose exec perforce sudo chmod 777 /p4
 ##### p4's maximum thread number is 10.
-docker-compose exec perforce p4 sync -q --parallel threads=8,batch=64 //sw/eris/... //sw/gpgpu/eris/cuda.vlcp
+docker-compose exec perforce p4 sync -q --parallel threads=10,batch=32 //sw/eris/... //sw/gpgpu/eris/cuda.vlcp
 docker-compose exec perforce sed -i '352 a\
                 args.append("--parallel")\
-                args.append("threads=8,batch=64")' /p4/sw/eris/src/common/perforce.py
-docker-compose exec perforce vulcan --sync ]=cublas_tests
+                args.append("threads=10,batch=32")' /p4/sw/eris/src/common/perforce.py
+docker-compose exec perforce vulcan --quiet --sync ]=cublas_tests
 docker-compose exec perforce vulcan --eris --install --target-revision=pkg-latest ]cuda
