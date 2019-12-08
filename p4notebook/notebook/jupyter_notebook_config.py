f8d51b4c24652c7b2841ad874ae59ca537414582
Your Name <you@example.com>
2019-12-08 14:15:52 +0800

    updated jupyter_notebook_config.py

--- p4notebook/notebook/jupyter_notebook_config.py
+++ p4notebook/notebook/jupyter_notebook_config.py
@@ -1,4 +1,8 @@
 c.NotebookApp.allow_root = True
 c.NotebookApp.ip = '0.0.0.0'
-from notebook.auth import passwd
+c.NotebookApp.open_browser = False
+c.NotebookApp.quit_button = False
+c.NotebookApp.websocket_compression_options = {}
+
+from notebook.auth import passwd;
 c.NotebookApp.password = passwd("example")
