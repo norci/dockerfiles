b0541536f7891b5459d600a39127aa2cf476d7e1
Your Name <you@example.com>
2020-07-19 06:49:44 +0000

    update julia_aarch64

--- julia_aarch64/init.jl
+++ julia_aarch64/init.jl
@@ -1,16 +1,6 @@
 import Pkg
 Pkg.update()
 Pkg.add.([
-    "BenchmarkTools",
-    "CSV",
-    "DataFrames",
-    "Flux",
-    "JuliaDB",
-    "JuliaDBMeta",
-    "Query",
-    "OnlineStats",
     "Revise",
-    "StatsPlots",
-    "Gadfly",
     ])
 Pkg.precompile()
