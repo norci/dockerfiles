71db0d5ecc0c9d3ad2759480930e177e5a303ab3
norci <norci@users.noreply.github.com>
2020-08-07 13:14:59 +0800

    Update startup.jl

--- julia_aarch64/startup.jl
+++ julia_aarch64/startup.jl
@@ -2,17 +2,21 @@
 # it must be set before launching julia. so set it in the system env.
 # ENV["JULIA_DEPOT_PATH"] = "D:\\ProgramData\\JULIA_DEPOT_PATH\\"
 ###
-ENV["JULIA_PKG_SERVER"] = "https://mirrors.bfsu.edu.cn/julia/static"
-ENV["JULIA_NUM_THREADS"] =  Base.Sys.CPU_THREADS
+# ENV["JULIA_PKG_SERVER"] = "https://mirrors.bfsu.edu.cn/julia/static" # too old
+ENV["JULIA_NUM_THREADS"] = Base.Sys.CPU_THREADS
 
-if ENV["OS"]!="Windows_NT"
+if ENV["OS"] != "Windows_NT"
     ENV["JULIA_EDITOR"] = "emacsclient"
 end
 
 try
     using Revise
+catch e
+    @warn(e.msg)
+end
 
-    if ENV["OS"]!="Windows_NT"
+try
+    if ENV["OS"] != "Windows_NT"
         using InteractiveCodeSearch
         InteractiveCodeSearch.CONFIG.interactive_matcher = `percol`
     end
