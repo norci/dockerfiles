a52470811ed78373074993725236431cc7861ccf
beann <1012252+beann@user.noreply.gitee.com>
2020-08-12 12:41:34 +0800

    update julia/startup.jl.

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,5 +1,9 @@
-ENV["JULIA_NUM_THREADS"] = Base.Sys.CPU_THREADS
+# The envs below must be set in system env
+# JULIA_NUM_THREADS
+# JULIA_DEPOT_PATH
+
 ENV["JULIA_CUDA_USE_BINARYBUILDER"] = false
+# ENV["JULIA_PKG_SERVER"] = "https://mirrors.bfsu.edu.cn/julia/static"
 
 try
     using Revise
@@ -8,10 +12,8 @@ catch e
 end
 
 try
-    # if ENV["OS"] != "Windows_NT"
     using InteractiveCodeSearch
     InteractiveCodeSearch.CONFIG.interactive_matcher = `peco`
-    # end
 catch e
     @warn(e.msg)
 end
