2c87d742bb27dbd3c23f6e743a3c456255443a32
Your Name <you@example.com>
2021-01-25 19:06:58 +0800

    add emacs in julia container

--- julia/up.sh
+++ julia/up.sh
@@ -7,17 +7,15 @@ ARGS="
 --gpus=all
 --interactive
 --name=julia
---privileged
 --tmpfs=/run:rw,exec
 --tmpfs=/tmp:rw,exec
 --tty
+--volume=$HOME/.ssh:/root/.ssh:ro
 --volume=/etc/localtime:/etc/localtime:ro
 --volume=/tmp/.X11-unix:/tmp/.X11-unix
---volume=`pwd`:/dockerfile:rw
 --volume=`pwd`/config:/root/.julia/config
---volume=$HOME/.ssh:/root/.ssh:ro
---volume=julia:/root/.julia
 --volume=code:/code
+--volume=julia:/root/.julia
 eordian/julia
 "
 
