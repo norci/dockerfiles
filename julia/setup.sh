89a59cf8360029e57f2d0c5147b4360e469400c5
norci <norci@users.noreply.github.com>
2021-01-31 11:14:49 +0800

    update setup.sh

--- julia/setup.sh
+++ julia/setup.sh
@@ -1,7 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -x
 sudo nvidia-smi -c EXCLUSIVE_PROCESS
-
-[ $(echo "get_server_list" | nvidia-cuda-mps-control) == ""  ] && sudo nvidia-cuda-mps-control -d
-
-
+sudo nvidia-cuda-mps-control -d
 xhost +local:root
