7ba8eee855a7e7f6bc03a13f7c57ac70dec31b97
norci <norci@users.noreply.github.com>
2021-02-17 18:35:27 +0800

    update CONDA_JL_HOME

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -4,11 +4,15 @@
 
 ENV["JULIA_PKG_SERVER"] = "https://mirrors.sjtug.sjtu.edu.cn/julia"
 
+@static if Sys.iswindows()
+    ENV["CONDA_JL_HOME"] = ∘(abspath, joinpath)(homedir(), raw"miniconda3\envs\jl")
+end
+
 @static if haskey(ENV, "CUDNN_VERSION")
     ENV["JULIA_CUDA_USE_BINARYBUILDER"] = false
 end
 
-for m in (:Revise, :Debugger, :InteractiveCodeSearch)
+for m in (:Revise, :Debugger)
     try
         eval(:(using $(m)))
     catch e
